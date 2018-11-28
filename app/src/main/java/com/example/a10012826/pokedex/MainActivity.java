2c24ebac6dd6c4fa94b1fc77a3cb0ed225956de7
unknown <saicmaduri@gmail.com>
2018-11-28 09:04:25 -0500

    Added Star and Saved List in Bundle

--- app/src/main/java/com/example/a10012826/pokedex/MainActivity.java
+++ app/src/main/java/com/example/a10012826/pokedex/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -19,6 +21,8 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.Serializable;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,6 +31,17 @@ public class MainActivity extends AppCompatActivity {
     ListView listView;
     TextView tvdescription;
     ArrayList<Pokemon> list;
+    int number;
+    public static final String POKEMON_LIST = "Pokemon List";
+    public static final String POKEMON_DESCRIPTION = "Pokemon Description";
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putSerializable(POKEMON_LIST, list);
+        outState.putSerializable(POKEMON_DESCRIPTION, number);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,15 +49,19 @@ public class MainActivity extends AppCompatActivity {
         list = new ArrayList<>();
         listView = findViewById(R.id.listView);
 
-        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            tvdescription = findViewById(R.id.tvdescription);
-            tvdescription.setMovementMethod(new ScrollingMovementMethod());
-        }
-
         list.add(new Pokemon("Bulbasaur", R.drawable.bulbasaur, "Fire", "Water", "Bulbasaur is a cute Pokémon born with a large seed firmly affixed to its back; the seed grows in size as the Pokémon does. Along with Squirtle and Charmander, Bulbasaur is one of the three Pokémon available at the beginning of Pokémon Red and Blue. It evolves into Ivysaur."));
         list.add(new Pokemon("Charmander", R.drawable.charmander, "Water", "Grass", "Charmander is a bipedal, reptilian Pokémon with a primarily orange body. Its underside from the chest down and soles are cream-colored. It has two small fangs visible in its upper jaw and two smaller fangs in its lower jaw. Charmander has blue eyes. Its arms and legs are short with four fingers and three clawed toes. A fire burns at the tip of this Pokémon's slender tail and has blazed there since Charmander's birth. The flame can be used as an indication of Charmander's health and mood, burning brightly when the Pokémon is strong, weakly when it is exhausted, wavering when it is happy, and blazing when it is enraged. It is said that Charmander dies if its flame goes out. However, if the Pokémon is healthy, the flame will continue to burn even if it gets a bit wet and is said to steam in the rain."));
         list.add(new Pokemon("Bulbasaur", R.drawable.squirtle, "Grass", "Fire", "It evolves into Wartortle starting at level 16, which evolves into Blastoise starting at level 36. Along with Bulbasaur and Charmander, Squirtle is one of three starter Pokémon of Kanto available at the beginning of Pokémon Red, Green, Blue, FireRed, and LeafGreen."));
 
+
+        if (savedInstanceState != null) {
+            list = (ArrayList<Pokemon>)savedInstanceState.getSerializable(POKEMON_LIST);
+        }
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            tvdescription = findViewById(R.id.tvdescription);
+            tvdescription.setMovementMethod(new ScrollingMovementMethod());
+        }
 
         CustomAdapter adapter = new CustomAdapter(this, R.layout.pokemon_layout, list);
 
@@ -74,10 +93,16 @@ public class MainActivity extends AppCompatActivity {
             final ImageView favorite = adapterLayout.findViewById(R.id.id_favorite);
             Button button = adapterLayout.findViewById(R.id.id_button);
 
-            favorite.setTag(R.drawable.staroff);
-
             textView.setText(list.get(position).getName());
             imageView.setImageResource(list.get(position).getImage());
+
+            if (list.get(position).getFavorite() == 1) {
+                favorite.setTag(R.drawable.staron);
+                favorite.setImageResource(R.drawable.staron);
+            } else {
+                favorite.setTag(R.drawable.staroff);
+                favorite.setImageResource(R.drawable.staroff);
+            }
 
             favorite.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -85,9 +110,11 @@ public class MainActivity extends AppCompatActivity {
                     if ((Integer)favorite.getTag() == R.drawable.staroff) {
                         favorite.setImageResource(R.drawable.staron);
                         favorite.setTag(R.drawable.staron);
+                        list.get(position).setFavorite(1);
                     } else {
                         favorite.setImageResource(R.drawable.staroff);
                         favorite.setTag(R.drawable.staroff);
+                        list.get(position).setFavorite(0);
                     }
                 }
             });
@@ -108,14 +135,14 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public class Pokemon {
+    public class Pokemon implements Serializable {
 
         String name;
         int image;
         String weaknesses;
         String strengths;
         String description;
-        boolean favorite;
+        int favorite;
 
         public Pokemon(String name, int image, String weaknesses, String strengths, String description) {
             this.name = name;
@@ -123,7 +150,7 @@ public class MainActivity extends AppCompatActivity {
             this.weaknesses = weaknesses;
             this.strengths = strengths;
             this.description = description;
-            favorite = false;
+            favorite = 0;
         }
 
         public String getName() {
@@ -146,11 +173,11 @@ public class MainActivity extends AppCompatActivity {
             return description;
         }
 
-        public boolean getFavorite() {
+        public int getFavorite() {
             return favorite;
         }
 
-        public void setFavorite(boolean favorite) {
+        public void setFavorite(int favorite) {
             this.favorite = favorite;
         }
     }
